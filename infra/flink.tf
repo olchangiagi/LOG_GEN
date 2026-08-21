16d6f134c398885cd3e2846fb41750dabee90a3c
olchangiagi <olchangiagi@gmail.com>
2026-08-21 16:35:45 +0900

    Feat: silver firehose 구성

--- infra/flink.tf
+++ infra/flink.tf
@@ -1,8 +1,8 @@
 # Managed Service for Apache Flink
 
 locals {
-    # flink 앱 경로. flink-silver.zip 파일은 스크립트에서 정의, flink 최종 산출물의 경로
-    # ${path.module} -> ~/infra (현재 디렉토리 위치)
+  # flink 앱 경로. flink-silver.zip 파일은 스크립트에서 정의, flink 최종 산출물의 경로
+  # ${path.module} -> ~/infra (현재 디렉토리 위치)
   flink_artifact_path = "${path.module}/../flink/target/flink-silver.zip"
   # zip 내용에 MD5 해시를 게산해 코드 변경 여부 식별 용도
   flink_artifact_hash = filemd5(local.flink_artifact_path)
@@ -13,9 +13,9 @@ resource "aws_s3_object" "flink_app" {
   # 버킷 지정
   bucket = aws_s3_bucket.data.id
   # flink app 지정 -> 키
-  key    = "flink/applications/flink-silver-${local.flink_artifact_hash}.zip"
+  key = "flink/applications/flink-silver-${local.flink_artifact_hash}.zip"
   # 로컬 -> s3 업로드한 zip 경로
-  source      = local.flink_artifact_path
+  source = local.flink_artifact_path
   # zip에 대한 해시 검사, 소스 변경되면 감지
   source_hash = local.flink_artifact_hash
   # 의존성, 사전에 aws_s3_bucket_public_access_block 완료된 후 진행
@@ -27,15 +27,15 @@ resource "aws_s3_object" "flink_app" {
 # flink 자체 내용
 resource "aws_kinesisanalyticsv2_application" "silver" {
   # flink 리소스 이름
-  name                   = local.flink_application_name
+  name = local.flink_application_name
   # 설명
-  description            = "Raw Kinesis events to Silver Kinesis using PyFlink"
+  description = "Raw Kinesis events to Silver Kinesis using PyFlink"
   # 런타임 환경
-  runtime_environment    = var.flink_runtime_environment
+  runtime_environment = var.flink_runtime_environment
   # role
   service_execution_role = aws_iam_role.flink.arn
   # app : true면 즉시 실행, false면 생성만 하고 실행은 직접 세팅
-  start_application      = var.flink_start_application
+  start_application = var.flink_start_application
 
   application_configuration {
     # 스냅샷 사용 x
@@ -51,7 +51,7 @@ resource "aws_kinesisanalyticsv2_application" "silver" {
           file_key   = aws_s3_object.flink_app.key
         }
       }
-      
+
       # 앱의 타임 zip
       code_content_type = "ZIPFILE"
     }
@@ -68,6 +68,7 @@ resource "aws_kinesisanalyticsv2_application" "silver" {
         }
       }
 
+      # silver kinesis, flink에서 출력하는 대상
       property_group {
         property_group_id = "OutputStream"
 
@@ -78,11 +79,15 @@ resource "aws_kinesisanalyticsv2_application" "silver" {
       }
 
       property_group {
+        # AWS Managed Flink를 인식하는 예약 그룹명 (실행 옵션) -> 고정값
         property_group_id = "kinesis.analytics.flink.run.options"
 
         property_map = {
-          "python"  = "main.py"
+          # flink 앱의 엔트리 포인트
+          "python" = "main.py"
+          # pyFlink에서 kinesis 접근 -> 드라이브(라이브러리) 필요 -> *.jar
           "jarfile" = "lib/pyflink-dependencies.jar"
+          # python UDF Worker가 transform.py를 import 하도록 등록
           "pyFiles" = "transform.py"
         }
       }
@@ -92,26 +97,26 @@ resource "aws_kinesisanalyticsv2_application" "silver" {
     flink_application_configuration {
       # 장애 복구시 checkpoint 설정
       checkpoint_configuration {
-        configuration_type = "DEFAULT"      # AWs 기본값을 따름
+        configuration_type = "DEFAULT" # AWs 기본값을 따름
       }
 
       # flink 로그, 매트릭 수집 수준 설정
       monitoring_configuration {
-        configuration_type = "CUSTOM"         # 직접 구성
-        log_level          = "INFO"           # 정보 수준
-        metrics_level      = "APPLICATION"    # 어플리케이션 단위(레벨) 정보
+        configuration_type = "CUSTOM"      # 직접 구성
+        log_level          = "INFO"        # 정보 수준
+        metrics_level      = "APPLICATION" # 어플리케이션 단위(레벨) 정보
       }
 
       # 어플리케이션 구동시 병렬 처리, KPU 설정
       parallelism_configuration {
-        configuration_type   = "CUSTOM"                     # 직접 설정
-        auto_scaling_enabled = true                         # 부하에 따라 자동 조절
-        parallelism          = var.flink_parallelism        # 기본값
-        parallelism_per_kpu  = var.flink_parallelism_per_kpu  # 기본값
+        configuration_type   = "CUSTOM"                      # 직접 설정
+        auto_scaling_enabled = true                          # 부하에 따라 자동 조절
+        parallelism          = var.flink_parallelism         # 기본값
+        parallelism_per_kpu  = var.flink_parallelism_per_kpu # 기본값
       }
     }
   }
-  
+
   # flink 실행 -> 로그 (정상, 에러) -> cloudwatch 저장
   cloudwatch_logging_options {
     log_stream_arn = aws_cloudwatch_log_stream.flink.arn
